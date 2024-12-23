6d5ef1f1ac146ced3c107d5930e4d016867ad19e
Jaeyong Kang <kjysmu@gmail.com>
2024-12-23 16:40:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,46 +3,69 @@ import logging
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from data_loader import JamendoDataModule
+from data_loader import DataModule
 from trainer import MusicClassifier
 from omegaconf import DictConfig
 import hydra
 from hydra.utils import to_absolute_path
 from hydra.core.hydra_config import HydraConfig
 from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.utilities.combined_loader import CombinedLoader
+from utilities.custom_early_stopping import MultiMetricEarlyStopping
 
 log = logging.getLogger(__name__)
 @hydra.main(version_base=None, config_path="config", config_name="train_config")
 def main(config: DictConfig):
+    is_mt = False
+    if "mt" in config.model.classifier:
+        is_mt = True
+
     logger = TensorBoardLogger("tb_logs", name="train_audio_classification")
     logger.log_hyperparams(config)
     train_log_dir = logger.log_dir
     log.info("Training starts")
 
-    data_module = JamendoDataModule( **config.dataset )
+    data_module = DataModule( config )
     data_module.setup()
-    trainloader = data_module.train_dataloader()
-    valloader = data_module.val_dataloader()
 
-    model = MusicClassifier( **config.model )
-    checkpoint_callback = ModelCheckpoint(monitor="val_loss",
-                                          filename="{epoch:02d}-{val_loss:.4f}",
-                                          save_top_k=2,
-                                          mode="min",
-                                          auto_insert_metric_name=False,
-                                          save_last=True
-                                          )
+    # Get the list of dataloaders for both train and validation, with dataset names
+    trainloaders = {dataset_name: loader for dataset_name, loader in zip(config.datasets, data_module.train_dataloader())}
+    vallowers = {dataset_name: loader for dataset_name, loader in zip(config.datasets, data_module.val_dataloader())}
+
+    # Combine multiple loaders using CombinedLoader, now with dataset names
+    combined_train_loader = CombinedLoader(trainloaders, mode="max_size")
+    combined_val_loader = CombinedLoader(vallowers, mode="max_size")
+
+    model = MusicClassifier( config )
+
+    if is_mt:
+        checkpoint_callback_mood = ModelCheckpoint(**config.checkpoint_mood)
+        checkpoint_callback_va = ModelCheckpoint(**config.checkpoint_va)
+        early_stop_callback = EarlyStopping(**config.earlystopping)
+        trainer = pl.Trainer(
+            **config.trainer,
+            callbacks=[checkpoint_callback_mood, checkpoint_callback_va, early_stop_callback],
+            logger=logger
+        )
+    else:
+        checkpoint_callback = ModelCheckpoint(**config.checkpoint)
+        early_stop_callback = EarlyStopping(**config.earlystopping)
+        trainer = pl.Trainer(
+            **config.trainer,
+            callbacks=[checkpoint_callback, early_stop_callback],
+            logger=logger
+        )
     
-    early_stop_callback = EarlyStopping(monitor='val_loss', patience=10, min_delta=0.0001, mode="min")
-    trainer = pl.Trainer(**config.trainer,
-                        callbacks=[checkpoint_callback, early_stop_callback],
-                        logger=logger)
-    trainer.fit(model, trainloader, valloader)
+    trainer.fit(model, combined_train_loader, combined_val_loader)
 
     if trainer.global_rank == 0:
         best_checkpoint_file = os.path.join(train_log_dir, 'best_checkpoint.txt')
         with open(best_checkpoint_file, 'w') as f:
-            f.write(f"Best checkpoint: {checkpoint_callback.best_model_path}\n")
+            if is_mt:
+                f.write(f"Best checkpoint (mood): {checkpoint_callback_mood.best_model_path}\n")
+                f.write(f"Best checkpoint (va): {checkpoint_callback_va.best_model_path}\n")
+            else:
+                f.write(f"Best checkpoint: {checkpoint_callback.best_model_path}\n")
             f.write(f"Version: {logger.version}\n")
 
 if __name__ == '__main__':
